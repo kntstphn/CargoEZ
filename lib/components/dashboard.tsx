9e25a8607a72217eb5cd9dacf74664129ca8f799
Kent Stephen Sumalinog <kentstephensumalinog@gmail.com>
2025-02-08 14:16:21 +0800

    Trying to fix the thing

--- lib/components/dashboard.tsx
+++ lib/components/dashboard.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 function Dashboard() {
   const containerRef = useRef<HTMLDivElement>(null);
